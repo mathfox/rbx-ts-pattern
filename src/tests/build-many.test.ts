3020f27855afedca9fe28f28f2871dbab9e05a8f
nonamie <53796817+mathfox@users.noreply.github.com>
2024-10-14 16:15:36 +0300

    tests

---
+++ src/tests/build-many.test.ts
@@ -0,0 +1,17 @@
+import { BuildMany } from "../types/BuildMany";
+import { Equal, Expect } from "../types/helpers";
+import { State } from "./types-catalog/utils";
+
+describe("BuildMany", () => {
+	it("should correctly update the content of a readonly tuple", () => {
+		type cases = [
+			Expect<Equal<BuildMany<readonly [number, State], [[{ status: "idle" }, [1]]]>, [number, { status: "idle" }]>>,
+			Expect<
+				Equal<
+					BuildMany<readonly [number, State], [[{ status: "idle" }, [1]]] | [[{ status: "loading" }, [1]]]>,
+					[number, { status: "idle" }] | [number, { status: "loading" }]
+				>
+			>,
+		];
+	});
+});
